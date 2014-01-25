38b8247f69ee282c581b2f65745da3d815bb8f73
darmok77 <sprosperi@tiscalinet.it>
2014-01-25 22:40:25 +0100

    Added Lazor, now with razor light!

---
+++ Assets/Weapons/LazorUse.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LazorUse : MonoBehaviour 
+{
+	bool lightActive = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnTriggerEnter()
+	{
+		if (!lightActive)
+		{
+			GameObject light = gameObject.transform.parent.FindChild ("Light").gameObject;
+			light.SetActive (true);
+			lightActive = true;
+		}
+	}
+
+	void OnTriggerExit()
+	{
+		if (lightActive)
+		{
+			GameObject light = gameObject.transform.parent.FindChild ("Light").gameObject;
+			light.SetActive (false);
+			lightActive = false;
+		}
+	}
+}
