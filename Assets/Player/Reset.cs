6d108762dfa2eaac489b0a08bb7dc54b11a879cd
parahunter <parahunter@gmail.com>
2014-01-26 10:06:18 +0000

    more stuff

---
+++ Assets/Player/Reset.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Reset : MonoBehaviour 
+{
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.R))
+			Application.LoadLevel(Application.loadedLevel);
+	}
+}
---
+++ Assets/TransitionIn/Transition.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Transition : MonoBehaviour 
+{
+	public AnimationCurve curve;
+
+	float scaleFactor = 0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
